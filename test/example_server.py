06a0d0a91299ce9f9a5785920a036500a1476414
Nate Cermak <nate.cermak@starfishneuro.com>
2024-03-15 18:46:43 -0500

    add screen capture to readme... lets see if it works

--- test/example_server.py
+++ test/example_server.py
@@ -1,16 +1,19 @@
 import socket
-import numpy
+import numpy as np
 import time
 
 server_socket = socket.socket()
-server_socket.bind(('localhost', 1234))
+server_socket.bind(('localhost', 12345))
 server_socket.listen()
 while True:
     conn, address = server_socket.accept()  # accept new connection
     counter = 0
     while True:
-        rand = numpy.random.rand() - 0.5
-        data = f"{counter}, {rand}, {rand:.5e}units {round(rand*1000)} junk\r\n"
+        x1 = np.random.rand() - 0.5
+        x2 = np.sin(time.time() * 100)
+        x3 = np.sin(time.time() * 10) + 0.1*x1
+        x4 = round(time.time() * 10) % 2
+        data = f"{counter}, {x1}, {x2:.5e}units {x3} junk {x4}\r\n"
 
         try:
             conn.send(data.encode('ascii'))
